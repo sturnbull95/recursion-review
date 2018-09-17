34df275f107ab38895d5f105fde760eba5d75faa
sturnbull95 <steven.turnbull901@gmail.com>
2018-09-17 14:12:38 -0700

    Solved StringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,55 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  /*
+  Checks:
+  undefined, null, boolean, string, number, array, object, function
+    
+AssertionError: expected '{"functions":,"undefined":undefined}' to equal '{}'
+
+
+
+
+
+  */  
+  var array = ['undefined','boolean','number'];
+  var resultArr = [];
+  if (array.includes(typeof obj)){
+    return String(obj);
+  }
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+  if (typeof obj === 'function') {
+    return "";
+  }
+  if (Array.isArray(obj)) {
+    if (obj.length == 0)
+            return '[]';
+        else {
+            obj.forEach(function(element) {
+                resultArr.push(stringifyJSON(element));
+            });
+            return '[' + resultArr + ']';
+        }
+  }
+
+  if(typeof obj === "object"){
+    if (obj === null) {
+      return 'null';
+    }
+    var result = '{';
+    for (var key in obj) {
+      if (typeof obj[key] === 'function' || typeof obj[key] === 'undefined') {
+        return '{}';
+      }
+      result += '"' + key + '":';
+      result += stringifyJSON(obj[key]) + ',';
+    }
+    if (result.charAt(result.length -1) === ',') {
+      result = result.substring(0,result.length -1);
+    }
+    result += '}';
+    return result;
+  }
 };
